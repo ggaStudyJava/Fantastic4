56c1d4781353bfb6a9abd39f3e18e05687e2d5ff
509-24 <509-24@509-24-PC>
2017-08-21 11:38:32 +0900

    집가자.

--- ex0821/src/ex0821/ex0821.java
+++ ex0821/src/ex0821/ex0821.java
@@ -4,6 +4,8 @@ public class ex0821 {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hi");
+		System.out.println("╬хЁГ");
+		System.out.println("╢ого©ю");
 		
 		
 	}
